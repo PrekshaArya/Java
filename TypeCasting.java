904735f9ec85325c5d1922721c2ea878c7a7305f
Preksha <aryapreksha@gmail.com>
2023-12-06 13:28:53 +0530

    First commit

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+class TypeCasting
+{
+ public static void main(String[] args)
+ {
+  int value1=5;
+  double value2=value1;
+  int value3= (int)value2;
+  System.out.println("Value 1: "+value1);
+  System.out.println("Value 2: "+value2);
+  System.out.println("Value 3: "+value3);
+ }
+}
